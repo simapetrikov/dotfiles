4e2dbd2f23c5336e4fe87dfdfe1fed529cb687c4
dm <>
2025-08-25 23:41:49 +0300

    added lsp and undotree to neovim

---
+++ .config/nvim/lua/boykisser/plugins/lsp.lua
@@ -0,0 +1,94 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+            }
+        }
+    },
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright" }, -- ставим Pyright
+        automatic_installation = true,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- Настройки для LSP (например, отключение подсветки при выделении)
+      local on_attach = function(client, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        local keymap = vim.keymap.set
+        keymap("n", "gd", vim.lsp.buf.definition, opts)
+        keymap("n", "K", vim.lsp.buf.hover, opts)
+        keymap("n", "gi", vim.lsp.buf.implementation, opts)
+        keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        keymap("n", "gr", vim.lsp.buf.references, opts)
+	keymap("n", "<leader>e", vim.diagnostic.open_float, { noremap = true, silent = true })
+	keymap("n", "[d", vim.diagnostic.goto_prev, { noremap = true, silent = true })
+	keymap("n", "]d", vim.diagnostic.goto_next, { noremap = true, silent = true })
+	keymap("n", "<leader>q", vim.diagnostic.setloclist, { noremap = true, silent = true })
+
+
+      end
+
+      -- Настройка Pyright
+      lspconfig.pyright.setup({
+        on_attach = on_attach,
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+}
